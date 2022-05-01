026b236650169ac52d17aa3abb34885c510dc78a
Damian <diqqqtv@gmail.comgit config --global user.email damian.mariusz.pyszka@gmail.comgit config --global user.name Damiangit commit -m created>
2022-05-01 21:42:45 +0200

    Created Magic 8Ball with JS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
-const priceInput = document.querySelector('.price')
-const peopleInput = document.querySelector('.people')
-const tipList = document.querySelector('.tip')
-const button = document.querySelector('.count')
-const errorInfo = document.querySelector('.error')
-const costInfo = document.querySelector('.cost-info')
-const costNumber = document.querySelector('.cost')
+const ball = document.querySelector('img')
+const input = document.querySelector('input')
+const answer = document.querySelector('.answer')
+const error = document.querySelector('.error')
 
-const fieldCheck = () => {
-    if(priceInput.value == '' || peopleInput.value == '' || tipList.value == 0) {
-        errorInfo.textContent = 'Uzupełnij wszystkie pola!'
-        costInfo.style.display = ''
-    } else {
-        errorInfo.textContent = ''
-        calculator()
-    }
+const answersArr = ['Tak', 'Nie wiadomo...', 'Nie']
+
+const checkInput = () => {
+	if (input.value === '') {
+		error.textContent = 'Musisz zadać jakieś pytanie!'
+		answer.textContent = ''
+	} else if (input.value.slice(-1) !== '?') {
+		error.textContent = 'Pytanie musi być zakończone znakiem "?".'
+		answer.textContent = ''
+	} else {
+		error.textContent = ''
+		randomAnswerGenerator()
+	}
+    ball.classList.remove('shake-animation')
 }
 
-const calculator = () => {
-    const newPrice = parseFloat(priceInput.value) // Tutaj też potrzebujemy parseFloat, ponieważ rachunki mogą mieć wartości po przecinku, więc nie powinniśmy otrzymywać wyłącznie liczby całkowitej
-    const newPeople = parseInt(peopleInput.value)
-    // const newTip = parseInt(tipList.value)
-    const newTip = parseFloat(tipList.value) // w wypadku tipList operujemy na value, czyli w HTML option i value -> 0.1, 0.15 itd. ParseInt zawsze zwraca nam liczbę całkowitą przez co np. 0.2 jest po prostu przybliżane do "0". Dlatego w tym wypadku musi być parseFloat, który
-
-    const sum =  (newPrice + (newPrice* newTip)) / newPeople
-
-    costInfo.style.display = 'block'
-    costNumber.textContent = sum.toFixed(2)
+const randomAnswerGenerator = () => {
+	const randomAnswer = Math.floor(Math.random() * answersArr.length)
+	answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[randomAnswer]}`
 }
 
-button.addEventListener('click', fieldCheck)
+const ballShake = () => {
+	ball.classList.add('shake-animation')
+    setTimeout(checkInput, 1000) 
+}
+
+ball.addEventListener('click', ballShake)
